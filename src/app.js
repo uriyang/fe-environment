a8c92c483146f50a8d3ec76dbe742ee40b72434e
uriyang <girlstoryk@gmail.com>
2023-05-18 06:30:03 +0900

    fix webpack es2015 modules

--- src/app.js
+++ src/app.js
@@ -1,1 +1,2 @@
-console.log(math.sum(1, 2));
+import * as math from "./math.js";
+console.log(math.sum(1, 2)); // 3
--- src/math.js
+++ src/math.js
@@ -1,8 +1,3 @@
-var math = math || {}; // math 네임스페이스
-
-(function () {
-  function sum(a, b) {
-    return a + b;
-  }
-  math.sum = sum; // 네이스페이스에 추가
-})();
+export function sum(a, b) {
+  return a + b;
+}

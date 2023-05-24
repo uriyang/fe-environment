c51a4d0922641466bff2098c2f335542e67729bd
uriyang <girlstoryk@gmail.com>
2023-05-25 06:17:26 +0900

    add webpack plugin

--- sample/webpack.config.js
+++ sample/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MyWebpackPlugin = require("./my-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -17,4 +18,5 @@ module.exports = {
       },
     ],
   },
+  plugins: [new MyWebpackPlugin()],
 };

91b95368df2f96e684d9426d464d178bfeccc105
uriyang <girlstoryk@gmail.com>
2023-05-25 06:29:18 +0900

    add emit tapasync

--- sample/my-webpack-plugin.js
+++ sample/my-webpack-plugin.js
@@ -3,6 +3,25 @@ class MyWebpackPlugin {
     compiler.hooks.done.tap("My Plugin", (stats) => {
       console.log("MyPlugin: done");
     });
+
+    // compiler.plugin() 함수로 후처리한다
+    compiler.hooks.emit.tapAsync("emit", (compilation, callback) => {
+      // const source = compilation.assets["main.js"].source();
+      // console.log(source);
+
+      compilation.assets["main.js"].source = () => {
+        const banner = [
+          "/**",
+          " * 이것은 BannerPlugin이 처리한 결과입니다.",
+          " * Build Date: 2019-10-10",
+          " */",
+          "",
+        ].join("\n");
+        return banner + "\n" + source;
+      };
+
+      callback();
+    });
   }
 }
 

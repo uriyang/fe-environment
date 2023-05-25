2744ab026ed7409009b5a014afbe276f431186c9
uriyang <girlstoryk@gmail.com>
2023-05-25 09:19:21 +0900

    fix const to var

--- sample/my-babel-plugin.js
+++ sample/my-babel-plugin.js
@@ -1,14 +1,13 @@
 module.exports = function myplugin() {
   return {
     visitor: {
-      Identifier(path) {
-        const name = path.node.name;
+      // https://github.com/babel/babel/blob/master/packages/babel-plugin-transform-block-scoping/src/index.js#L26
+      VariableDeclaration(path) {
+        console.log("VariableDeclaration() kind:", path.node.kind); // const
 
-        // 바벨이 만든 AST 노드를 출력한다
-        console.log("Identifier() name:", name);
-
-        // 변환작업: 코드 문자열을 역순으로 변환한다
-        path.node.name = name.split("").reverse().join("");
+        if (path.node.kind === "const") {
+          path.node.kind = "var";
+        }
       },
     },
   };
